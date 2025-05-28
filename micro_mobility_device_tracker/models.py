52c2d2bb15a3cb8ec96b9a205001911b096c0433
33sihan <siw031@ucsd.edu>
2025-05-27 17:43:35 -0700

    Updated models and added migration

--- micro_mobility_device_tracker/models.py
+++ micro_mobility_device_tracker/models.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+import uuid
+import os
+
+def user_image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{instance.user.username}_{uuid.uuid4().hex}.{ext}"
+    return os.path.join('owner_photos/', filename)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    owner_image = models.ImageField(upload_to='owner_photos/', null=True, blank=True)
+    owner_image = models.ImageField(upload_to=user_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
